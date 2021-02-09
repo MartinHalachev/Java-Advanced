ee8909b2a6b08c14ae652c2b1e113411045ba1ea
MartinHalachev <75757581+MartinHalachev@users.noreply.github.com>
2021-02-09 10:19:08 +0200

    up

---
+++ OOP_Exercises/birthdayCelebrations/Main.java
@@ -0,0 +1,13 @@
+package birthdayCelebrations;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] readInput = scanner.nextLine().split(" ");
+    }
+}
